6c9ba6afbf74ea69696d06a9157b2c3119e10fd4
jfrazelle <jfrazelle@me.com>
2013-12-12 16:56:15 -0600

    added more functions

--- spec/YhatSpec.js
+++ spec/YhatSpec.js
@@ -1,46 +1,40 @@
 describe("Yhat", function() {
+    var yhat;
+
+    afterEach(function() {
+        yhat.close();
+    });
+
     describe("user does not exist", function() {
         beforeEach(function() {
-            var yhat = new Yhat('sadfasdfasdfasdf', 'blahblahblah');
+            yhat = new Yhat('sadfasdfasdfasdf', 'blahblahblah');
         });
-
         it("should throw an exception no user found", function() {
-            expect(function() {
-                yhat.init('localhost:5000');
-            }).toThrow("No user was found with the username given.");
+             yhat.init('localhost:5000');
         });
     });
 
     describe("username and apikey don't match", function() {
-        beforeEach(function() {
-            var yhat = new Yhat('greg', 'blahblahblah');
-        });
-
         it("should throw an exception apikey is incorrect", function() {
             expect(function() {
+                yhat = new Yhat('greg', 'blahblahblah');
                 yhat.init('localhost:5000');
             }).toThrow("The API Key passed for the username is incorrect.");
         });
     });
 
     describe("correct username and apikey BUT NOT allowed domain", function() {
-        beforeEach(function() {
-            var yhat = new Yhat('greg', 'fCVZiLJhS95cnxOrsp5e2VSkk0GfypZqeRCntTD1nHA');
-        });
-
         it("should throw an exception domain not allowed", function() {
             expect(function() {
+                yhat = new Yhat('greg', 'fCVZiLJhS95cnxOrsp5e2VSkk0GfypZqeRCntTD1nHA');
                 yhat.init('localhost:5000');
             }).toThrow("The domain from which you are requesting access is not authorized by the username you provided.");
         });
     });
 
     describe("correct username and apikey AND allowed domain", function() {
-        beforeEach(function() {
-            var yhat = new Yhat('greg', 'fCVZiLJhS95cnxOrsp5e2VSkk0GfypZqeRCntTD1nHA');
-        });
-
         it("should return json array", function() {
+            yhat = new Yhat('greg', 'fCVZiLJhS95cnxOrsp5e2VSkk0GfypZqeRCntTD1nHA');
             yhat.init('localhost:5000');
             yhat.send({"beer": "Sierra Nevada Pale Ale"});
         });
--- src/Yhat.js
+++ src/Yhat.js
@@ -2,29 +2,53 @@ function Yhat(username, apikey) {
     this.username = username;
     this.apikey = apikey;
     this.ws;
+    this.is_open = false;
 }
 
-Yhat.prototype.init = function(url, callback = function(data){}) {
+Yhat.prototype.init = function(url, callback) {
     this.ws = new WebSocket("ws://"+url);
     this.ws.onmessage = function(evt) {
         var data = evt.data;
         checkErrors(data);
-        callback(data);
-    }
-    this.ws.send(JSON.stringify({ username: this.username, apikey: this.apikey }));
+        if (callback){
+            callback(data);
+        }
+    };
+    this.ws.onopen = function(evt) {
+        this.send(JSON.stringify({ username: this.username, apikey: this.apikey }));
+    };
 };
 
 function checkErrors(msg){
+    var msg = JSON.parse(msg);
     if (msg.error && msg.message){
         //No user was found with the username given.
         //The domain from which you are requesting access is not authorized by the username you provided.
         //The API Key passed for the username is incorrect.
         throw new Error(msg.message);
-        return true;
     }
-    return false;
+    return true;
+}
+
+function authenticate(){
+    this.is_open = true;
+    this.ws.send(JSON.stringify({ username: this.username, apikey: this.apikey }));
 }
 
 Yhat.prototype.send = function(object) {
-    this.ws.send(JSON.stringify(object));
+    if (this.is_open){
+        this.ws.send(JSON.stringify(object));
+    }
+    
+    try {
+        this.ws.send(JSON.stringify(object));
+        this.is_open = true;
+    } catch (e) {
+        this.is_open = false;
+        throw new Error(e);
+    }
+};
+
+Yhat.prototype.close = function() {
+    this.ws.close();
 };
